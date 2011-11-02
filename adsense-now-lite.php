ae64310725a0198ec42ed2e5c5869fcaa2dc98c3
manojtd <manojtd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-02 22:30:00 +0000

    Initial release of the Lite version
    git-svn-id: https://plugins.svn.wordpress.org/adsense-now-lite/trunk@458800 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adsense-now-lite.php
@@ -0,0 +1,365 @@
+<?php
+/*
+Plugin Name: AdSense Now! Lite
+Plugin URI: http://www.thulasidas.com/adsense
+Description: Get started with AdSense now, and make money from your blog. Configure it at <a href="options-general.php?page=adsense-now-lite.php">Settings &rarr; AdSense Now! Lite</a>.
+Version: 3.00
+Author: Manoj Thulasidas
+Author URI: http://www.thulasidas.com
+*/
+
+/*
+Copyright (C) 2008 www.thulasidas.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+if (!class_exists("adsNow")) {
+  class adsNow {
+    var $plugindir, $locale, $defaults, $adminOptions, $adminOptionName, $mcAd;
+    function adsNow() { //constructor
+      if (file_exists (dirname (__FILE__).'/defaults.php')){
+        include (dirname (__FILE__).'/defaults.php');
+        $this->defaults = $defaults ;
+      }
+      if (empty($this->defaults))  {
+        add_action('admin_notices', create_function('', 'if (substr( $_SERVER["PHP_SELF"], -11 ) == "plugins.php"|| $_GET["page"] == "adsense-now-lite.php") echo \'<div class="error"><p><b><em>AdSense Now!</em></b>: Error locating or loading the defaults! Ensure <code>defaults.php</code> exists, or reinstall the plugin.</p></div>\';')) ;
+      }
+    }
+    function init() {
+      $this->getAdminOptions();
+    }
+    //Returns an array of admin options
+    function getAdminOptions($reset = false) {
+      if (!$reset && count($this->adminOptions) > 0) {
+        return $this->adminOptions ;
+      }
+      $mThemeName = get_settings('stylesheet') ;
+      $mOptions = "adsNow" . $mThemeName ;
+      $this->plugindir = get_option('siteurl') . '/' . PLUGINDIR .
+        '/' . basename(dirname(__FILE__)) ;
+      $locale = get_locale();
+      $this->locale = $locale ;
+      if(!empty($this->locale) && $this->locale != 'en_US') {
+        $moFile = dirname(__FILE__) . '/lang/' . $this->locale . '/easy-adsenser.mo';
+        if(@file_exists($moFile) && is_readable($moFile))
+          load_textdomain('easy-adsenser', $moFile);
+        else {
+          // look for any other similar locale with the same first three characters
+          $foo = glob(dirname(__FILE__) . '/lang/' . substr($this->locale, 0, 2) .
+                      '*/easy-adsenser.mo') ;
+          if (!empty($foo)) {
+            $moFile = $foo[0] ;
+            load_textdomain('easy-adsenser', $moFile);
+            $this->locale = basename(dirname($moFile)) ;
+          }
+        }
+      }
+      $adsNowAdminOptions =
+        array('info' => "<!-- AdSense Now Lite V2.03 -->\n",
+          'policy' => 'unknown',
+          'ad_text' => $this->defaults['defaultText'],
+          'show_leadin' => 'float:right',
+          'show_midtext' => 'float:left',
+          'show_leadout' => 'float:right',
+          'kill_pages' => false,
+          'kill_home' => false,
+          'kill_attach' => false,
+          'kill_front' => false,
+          'kill_cat' => false,
+          'kill_tag' => false,
+          'kill_archive' => false);
+
+      $adNwOptions = get_option($mOptions);
+      if (empty($adNwOptions)) {
+        // try loading the default from the pre 1.3 version, so as not to annoy
+        // the dudes who have already been using adNwsenser
+        $adminOptionsName = "adsNowAdminOptions";
+        $adNwOptions = get_option($adminOptionsName);
+      }
+      if (!empty($adNwOptions) && ! $reset) {
+        foreach ($adNwOptions as $key => $option)
+          $adsNowAdminOptions[$key] = $option;
+      }
+
+      update_option($mOptions, $adsNowAdminOptions);
+      $this->adminOptions = $adsNowAdminOptions ;
+      $this->adminOptionName = $mOptions ;
+      return $adsNowAdminOptions;
+    }
+
+    function handleDefaultText($text, $key = '300x250') {
+      $ret = $text ;
+      if ($ret == $this->defaults['defaultText']
+        || strlen(trim($ret)) == 0) {
+        $x = strpos($key, 'x') ;
+        $w = substr($key, 0, $x);
+        $h = substr($key, $x+1);
+        $p = (int)(min($w,$h)/6) ;
+        $ret = '<div style="width:'.$w.'px;height:'.$h.'px;border:1px solid red;"><div style="padding:'.$p.'px;text-align:center;font-family:arial;font-size:8pt;"><p>Your ads will be inserted here by</p><p><b><a href="http://buy.ads-ez.com/adsense-now" title="The simplest and quickest AdSense Plugin for WordPress" target="_blank">AdSense Now! Lite</a></b>.</p><p>Please go to the plugin admin page to paste your ad code.</p></div></div>' ;
+      }
+      return $ret ;
+    }
+
+    //Prints out the admin page
+    function printAdminPage() {
+      if (empty($this->defaults)) return ;
+      $mThemeName = get_settings('stylesheet') ;
+      $mOptions = "adsNow" . $mThemeName ;
+      $adNwOptions = $this->getAdminOptions();
+
+      if (isset($_POST['update_adsNowSettings'])) {
+        if (isset($_POST['ezAdSensePolicy']))
+          $adNwOptions['policy'] = $_POST['ezAdSensePolicy'];
+        if (isset($_POST['adsNowText'])) {
+          $adNwOptions['ad_text'] = $_POST['adsNowText'];
+        }
+        if (isset($_POST['adsNowShowLeadin'])) {
+          $adNwOptions['show_leadin'] = $_POST['adsNowShowLeadin'];
+        }
+        if (isset($_POST['adsNowShowMidtext'])) {
+          $adNwOptions['show_midtext'] = $_POST['adsNowShowMidtext'];
+        }
+        if (isset($_POST['adsNowShowLeadout'])) {
+          $adNwOptions['show_leadout'] = $_POST['adsNowShowLeadout'];
+        }
+        $adNwOptions['kill_pages'] = $_POST['adNwKillPages'];
+        $adNwOptions['kill_home'] = $_POST['adNwKillHome'];
+        $adNwOptions['kill_attach'] = $_POST['adNwKillAttach'];
+        $adNwOptions['kill_front'] = $_POST['adNwKillFront'];
+        $adNwOptions['kill_cat'] = $_POST['adNwKillCat'];
+        $adNwOptions['kill_tag'] = $_POST['adNwKillTag'];
+        $adNwOptions['kill_archive'] = $_POST['adNwKillArchive'];
+
+        $adNwOptions['info'] = $this->info() ;
+
+        update_option($mOptions, $adNwOptions);
+
+        echo '<div class="updated"><p><strong>';
+        _e("Settings Updated.", "easy-adsenser");
+        echo '</strong></p> </div>' ;
+      }
+      else if (isset($_POST['reset_adsNowSettings'])) {
+        $reset = true ;
+        $adNwOptions = $this->getAdminOptions($reset);
+        echo '<div class="updated"><p><strong>' ;
+        _e("Ok, all your settings have been discarded!","easy-adsenser");
+        echo '</strong></p> </div>' ;
+      }
+      else if (isset($_POST['clean_db']) || isset($_POST['kill_me'])) {
+        $reset = true ;
+        $adNwOptions = $this->getAdminOptions($reset);
+        $this->cleanDB('adsNow');
+
+        echo '<div class="updated"><p><strong>' ;
+        _e("Database has been cleaned. All your options for this plugin (for all themes) have been removed.",
+           "easy-adsenser");
+        echo '</strong></p> </div>' ;
+        if (isset($_POST['kill_me'])) {
+          remove_action('admin_menu', 'adsNow_ap') ;
+          deactivate_plugins('adsense-now-lite/adsense-now-lite.php', true);
+
+          echo '<div class="updated"><p><strong>' ;
+          _e("This plugin has been deactivated.", "easy-adsenser");
+          echo '<a href="plugins.php?deactivate=true">' ;
+          _e("Refresh", "easy-adsenser") ;
+          echo '</a></strong></p></div>' ;
+
+          return;
+        }
+      }
+      if (file_exists (dirname (__FILE__).'/admin.php'))
+        include (dirname (__FILE__).'/admin.php');
+      else
+      echo '<font size="+1" color="red">' .
+        __("Error locating the admin page!\nEnsure admin.php exists, or reinstall the plugin.", 'easy-adsenser') .
+        '</font>' ;
+    }//End function printAdminPage()
+
+    function mc($mc, $ad, $size=false, $key='300x250') {
+      return $ad ;
+    }
+
+    function info() {
+      $me = basename(dirname(__FILE__)) . '/' . basename(__FILE__);
+      $plugins = get_plugins() ;
+      $str =  "<!-- " . $plugins[$me]['Title'] . " V" . $plugins[$me]['Version'] . " -->\n";
+      return $str ;
+    }
+
+    var $nwMax = 3 ;
+    var $mced = false ;
+
+    function cleanDB($prefix){
+      global $wpdb ;
+      $wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE '$prefix%'") ;
+    }
+
+    function plugin_action($links, $file) {
+      if ($file == plugin_basename(dirname(__FILE__).'/adsense-now-lite.php')){
+      $settings_link = "<a href='options-general.php?page=adsense-now-lite.php'>" .
+        __('Settings', 'easy-adsenser') . "</a>";
+      array_unshift( $links, $settings_link );
+      }
+      return $links;
+    }
+
+    function contentMeta() {
+      $adNwOptions = $this->getAdminOptions();
+      global $post;
+      $meta = get_post_custom($post->ID);
+      $adkeys = array('adsense', 'adsense-top', 'adsense-middle', 'adsense-bottom') ;
+      $ezkeys = array('adsense', 'show_leadin', 'show_midtext', 'show_leadout') ;
+      $metaOptions = array() ;
+      // initialize to adNwOptions
+      foreach ($ezkeys as $key => $optKey) {
+        $metaOptions[$ezkeys[$key]] = $adNwOptions[$optKey] ;
+      }
+      // overwrite with custom fields
+      if (!empty($meta)) {
+        foreach ($meta as $key => $val) {
+          $tkey = array_search(strtolower(trim($key)), $adkeys) ;
+          if ($tkey !== FALSE) {
+            $value = strtolower(trim($val[0])) ;
+            // ensure valid values for options
+            if ($value == 'left' || $value == 'right' || $value == 'center' || $value == 'no') {
+              if ($value == 'left' || $value == 'right') $value = 'float:' . $value ;
+              if ($value == 'center') $value = 'text-align:' . $value ;
+              $metaOptions[$ezkeys[$tkey]] = $value ;
+            }
+          }
+        }
+      }
+      return $metaOptions ;
+    }
+
+    function adsNow_content($content) {
+      $adNwOptions = $this->getAdminOptions();
+      if ($adNwOptions['kill_pages'] && is_page()) return $content ;
+      if ($adNwOptions['kill_home'] && is_home()) return $content ;
+      if ($adNwOptions['kill_attach'] && is_attachment()) return $content ;
+      if ($adNwOptions['kill_front'] && is_front_page()) return $content ;
+      if ($adNwOptions['kill_cat'] && is_category()) return $content ;
+      if ($adNwOptions['kill_tag'] && is_tag()) return $content ;
+      if ($adNwOptions['kill_archive'] && is_archive()) return $content ;
+      $mc = $adNwOptions['mc'] ;
+      $this->mced = false ;
+      global $nwCount ;
+      if ($nwCount >= $this->nwMax) return $content ;
+      if(strpos($content, "<!--noadsense-->") !== false) return $content;
+      $metaOptions = $this->contentMeta() ;
+      if ($metaOptions['adsense'] == 'no') return $content;
+
+      $show_leadin = $metaOptions['show_leadin'] ;
+      $leadin = '' ;
+      if ($show_leadin != 'no')
+      {
+        if ($nwCount < $this->nwMax)
+        {
+          $nwCount++;
+          $adText = $this->handleDefaultText($adNwOptions['ad_text']) ;
+          $leadin =
+            stripslashes($adNwOptions['info'] . "<!-- Post[count: " . $nwCount . "] -->\n" .
+                         '<div class="adsense adsense-leadin" style="' .
+                         $show_leadin . ';margin: 12px;">' .
+                         $this->mc($mc, $adText) . '</div>') ;
+        }
+      }
+
+      $show_midtext = $metaOptions['show_midtext'] ;
+      if ($show_midtext != 'no')
+      {
+        if ($nwCount < $this->nwMax)
+        {
+          $poses = array();
+          $lastpos = -1;
+          $repchar = "<p";
+          if(strpos($content, "<p") === false)
+            $repchar = "<br";
+
+          while(strpos($content, $repchar, $lastpos+1) !== false){
+            $lastpos = strpos($content, $repchar, $lastpos+1);
+            $poses[] = $lastpos;
+          }
+          $half = sizeof($poses);
+          while(sizeof($poses) > $half)
+            array_pop($poses);
+          $pickme = $poses[floor(sizeof($poses)/2)];
+          $nwCount++;
+          $adText = $this->handleDefaultText($adNwOptions['ad_text']) ;
+          $midtext =
+            stripslashes($adNwOptions['info'] . "<!-- Post[count: " . $nwCount . "] -->\n" .
+                         '<div class="adsense adsense-midtext" style="' .
+                         $show_midtext . ';margin: 12px;">' .
+                         $this->mc($mc, $adText) . '</div>') ;
+          $content = substr_replace($content, $midtext.$repchar, $pickme, 2);
+        }
+      }
+
+      $show_leadout = $metaOptions['show_leadout'] ;
+      $leadout = '' ;
+      if ($show_leadout != 'no')
+      {
+        if ($nwCount < $this->nwMax)
+        {
+          $nwCount++;
+          $adText = $this->handleDefaultText($adNwOptions['ad_text']) ;
+          $leadout =
+            stripslashes($adNwOptions['info'] . "<!-- Post[count: " . $nwCount . "] -->\n" .
+                         '<div class="adsense adsense-leadout" style="' .
+                         $show_leadout . ';margin: 12px;">' .
+                         $this->mc($mc, $adText) . '</div>') ;
+        }
+      }
+
+      return $leadin . $content . $leadout ;
+    }
+  }
+} //End Class adsNow
+
+$nwCount = 0 ;
+
+// provide a replacement for htmlspecialchars_decode() (for PHP4 compatibility)
+if (!function_exists("htmlspecialchars_decode")) {
+  function htmlspecialchars_decode($string,$style=ENT_COMPAT) {
+    $translation = array_flip(get_html_translation_table(HTML_SPECIALCHARS,$style));
+    if($style === ENT_QUOTES){ $translation['&#039;'] = '\''; }
+    return strtr($string,$translation);
+  }
+}
+
+if (class_exists("adsNow")) {
+  $nw_ad = new adsNow();
+  if (isset($nw_ad) && !empty($nw_ad->defaults)) {
+    //Initialize the admin panel
+    if (!function_exists("adsNow_ap")) {
+      function adsNow_ap() {
+        global $nw_ad ;
+        if (function_exists('add_options_page')) {
+          $mName = 'AdSense Now! Lite' ;
+          add_options_page($mName, $mName, 9, basename(__FILE__),
+            array(&$nw_ad, 'printAdminPage'));
+        }
+      }
+    }
+
+    add_filter('the_content', array($nw_ad, 'adsNow_content'));
+    add_action('admin_menu', 'adsNow_ap');
+    add_action('activate_' . basename(dirname(__FILE__)) . '/' . basename(__FILE__),
+               array(&$nw_ad, 'init'));
+    add_filter('plugin_action_links', array($nw_ad, 'plugin_action'), -10, 2);
+  }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,130 @@
+=== AdSense Now! Lite ===
+Contributors: manojtd
+Donate link: http://buy.ads-ez.com/adsense-now
+Tags: adsense, ad, ads, advertising, google
+Requires at least: 2.5
+Tested up to: 3.2
+Stable tag: 3.00
+
+AdSense Now! gets you started with Google AdSense. No mess, no fuss.
+
+== Description ==
+
+*AdSense Now!* is the simplest possible way to generate revenue from your blog using Google AdSense. Aiming at simplicity, *AdSense Now!* does only one thing: it puts your AdSense code in up to three spots in your posts and pages (both existing ones and those yet to be written).
+
+= Features =
+
+1. Enforces the Google policy of not more than three ad blocks per page.
+1. Simplest possible configuration interface -- nothing more than cutting and pasting AdSense code.
+1. Option to suppress ads on all pages (as opposed to posts), or on the front/home page., category/tag/archive listings.
+1. Control over the positioning and display of AdSense blocks in each post or page.
+1. Internationalized with multiple language support.
+
+PS: You'll need a [Google AdSense Account](http://adsense.google.com/).
+
+*AdSense Now! Lite* is the freely distributed version of a premium plugin. The [Pro version](http://buy.ads-ez.com/adsense-now/ "Pro version of the AdSense Now! plugin for only $3.95") gives you more control. It gives you filter to ensure that your ads show only on those pages that seem to comply with Google AdSense policies, which can be important since some comments may render your pages inconsistent with those policies. It also lets you specify a list of computers where your ads will not be shown, in order to prevent accidental clicks on your own ads -- one of the main reasons AdSense bans you. These features will minimize your chance of getting banned. Also in the works for the Pro version is a compatibility mode, which solves the issue of the ad insertion messing up your page appearances when using some  themes. The Pro version costs $3.95, and is appropriate if you expect to make more than $75 of ad revenue from your site.
+
+If you think *AdSense Now!* is too lean and mean for your taste, try the full-fledged, feature rich plugin [Easy AdSense Pro](http://buy.ads-ez.com/easy-adsense "The complete solution for all things AdSense related").
+
+Want to go beyond AdSense? Use my new plugin [Easy Ads](http://www.thulasidas.com/plugins/easy-ads/ "A new plugin to handle AdSense and its alternatives") that can handle [Clicksor](http://www.clicksor.com/pub/index.php?ref=105268 "Careful, don't double-date with AdSense and Clicksor, they get very jealous of each other!"), [BidVertiser](http://www.bidvertiser.com/bdv/bidvertiser/bdv_ref_publisher.dbm?Ref_Option=pub&Ref_PID=229404 "Another fine ad provider") or [Chitika](http://chitika.com/publishers.php?refid=manojt "Compatible with AdSense") and, of course, AdSense! In fact, the AdSense part of Easy Ads is very similar to *AdSense Now!*
+
+If you like *AdSense Now!*, you may want to check out my other plugins: [Theme Tweaker](http://www.thulasidas.com/plugins/theme-tweaker/ "To tweak the colors in your theme with no CSS/PHP editing") and [Easy LaTeX](http://www.thulasidas.com/plugins/easy-latex/ "To display mathematical equations in your blog using LaTeX"). And my plugin for plugin authors: [Easy Translator](http://www.thulasidas.com/plugins/easy-translator/  "To translate any plugin (with internationalized strings) to your language.").
+
+= New in 3.00 =
+
+Initial release of the Lite version.
+
+== Upgrade Notice ==
+
+= 3.00 =
+
+Initial release of the Lite version.
+
+== Screenshots ==
+
+1. How to set the options for *AdSense Now!*
+
+== Installation ==
+
+1. Upload the *AdSense Now! Lite* plugin (the whole adsense-now-lite folder) to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to the Setup -> AdSense Now! and enter your AdSense code and options.
+
+== Frequently Asked Questions ==
+
+= Why do I get a "Plugin does not have valid header" error? =
+
+This seems to be a problem with some WordPress installations. I have never been able to reproduce this error on any of my installations. I have found [this on the web](http://webdesign.anmari.com/2312/activation-error-plugin-does-not-have-valid-header-still-activates/ "This may give you some ideas") though.
+
+= How can I control the appearance of the adsense blocks using CSS? =
+
+All `<div>`s that *AdSense Now!* creates have the class attribute `adsense`. Furthermore, they have attributes like `adsense-leadin`, `adsense-midtext` and `adsense-leadout` depending on the type. You can set the style for these classes in your theme `style.css` to control their appearance.
+
+= Why another AdSense plugin? =
+
+The other plugin I have published, [Easy AdSense](http://buy.ads-ez.com/easy-adsense "The complete solution for all things AdSense related"), does everything that *AdSense Now!* does and more. But in doing so, Easy AdSense has become an All-in-One AdSense plugin, and not "Easy" any more. I wanted to go back to the basics and provide a simple plugin to get our fellow bloggers started on AdSense.
+
+= I like its simplicity, but *AdSense Now!* doesn't have all the features I need. Can you add *this* and *that* feature? =
+
+If you think *AdSense Now!* is too lean and mean for your taste, try my full-fledged, feature-rich plugin [Easy AdSense](http://buy.ads-ez.com/easy-adsense "The complete solution for all things AdSense related"). I plan to keep *AdSense Now!* simple.
+
+= I just activated the plugin. How come I don't see any ads in my blog? =
+
+Note that you have to generate your adsense code from Google, and paste the *entire* code in the text box, replacing the existing text. Changing the publisher id alone is not good enough.
+
+If you just created the new Google AdSense code, it may not be active yet. Google takes about ten minutes or so before serving ads. Please try again later.
+
+= Can I control how the ad blocks are formatted in each page? =
+
+Yes! Now, in V1.1+, you more options [through **custom fields**] to control ad blocks in individual posts/pages. Add custom fields with keys like **adsense-top, adsense-middle, adsense-bottom, adsense-widget, adsense-search** and with values like **left, right, center** or **no** to have control how the ad blocks show up in each post or page. The value "**no**" suppresses all AdSense ad blocks in the post or page.
+
+= How do I report a bug or ask a question? =
+
+Please report any problems, and share your thoughts and comments [at the plugin forum at WordPress](http://wordpress.org/tags/adsense-now-lite "Post comments/suggestions/bugs on the WordPress.org forum. [Requires login/registration]") Or send an [email to the plugin author](http://manoj.thulasidas.com/mail.shtml "Email the author").
+
+== Change Log ==
+
+* V3.00: Initial release of the Lite version. [Nov 1, 2011]
+* V2.05: Documentation and admin-page display changes. Non-critical. [Aug 30, 2011]
+* V2.04: Bug fix. [Aug 29, 2011]
+* V2.00: Documentation changes. [July 4, 2011]
+* V1.99: Multiple translation updates and changes to default ads. Ensured compatibility with WordPress 3.2. [June 17, 2011]
+* V1.98: More translation updates and support options, and Admin interface changes. [Oct 14, 2010]
+* V1.97: Changing default and shared ads, and translation update (`pt_PT`). [Oct 2, 2010]
+* V1.96: Translation updates. Minor bug fixes and enhancements. [Sep 16, 2010]
+* V1.95: Documentation (FAQ) and defaults changes. [Aug 18, 2010]
+* V1.94: Performance optimization, , updating some translations, and changing the default and shared ads (to publicize the author's books  and to include other providers) [Aug 12, 2010]
+* V1.92: Major revamping of default ads: The default (and shared) ads have been changed to referral images. The text boxes on the admin page now show a reminder to generate and paste your AdSense code. [June 27, 2010]
+* V1.90: Updated the Russian translation. [Apr 10, 2010]
+* V1.89: Updating many translations. [Mar 20, 2010]
+* V1.83: New and updated translations. [Dec 10, 2009]
+* V1.80: Releasing new Ukrainian and Polish, and updated Italian translations. [Sept 24, 2009]
+* V1.77: Releasing new Arabic, and updated German, French and Indonesian translations. [August 22, 2009]
+* V1.75: Compressing the defaults file. No functional improvements. [August 10, 2009]
+* V1.74: A few more translations. (English users do not need to update.) [August 5, 2009]
+* V1.64: Releasing `pt_PT` translation. [July 22, 2009]
+* V1.63: New translation in Russian (`ru_RU`). Updated translations: Portuguese (`pt_BR`), Simplified Chinese (`zh_CN`). [July 18, 2009]
+* V1.62: Attempts to fix automatic update and install issues. Improved documentation highlighting ad space sharing. Not an essential update, but any feedback will be appreciated. [July 16, 2009]
+* V1.61: Changes to streamline internationalization. (English users don't need to update.) [July 14, 2009]
+* V1.58: Admin page enhancements. Simplified Chinese translation. [July 2, 2009]
+* V1.57: Removing min-max enforcing on ad space sharing. [June 30, 2009]
+* V1.56: Fixing a typo in `is_category()`. [June 26, 2009]
+* V1.55: Fixing the issue with submit buttons in IE8. [June 23, 2009]
+* V1.54: Providing `htmlspecialchars_decode()` for compatibility with older versions of PHP. [June 23, 2009]
+* V1.53: The `<div>` containing the adsense code has class names set so that they can be controlled from the theme CSS. The shared ad-slots are of the same size and show only text ads now. [June 23, 2009]
+* V1.52: German translation. [June 20,2009]
+* V.151: Compatibility with WP2.8+. [June 13, 2009]
+* V1.50: Interface streamlining and improvements, minor bug fixes and configurable ad space sharing to support the plugin development. [June 12, 2009]
+* V1.37: Option to suppress ad blocks in front/home/category/tag/archive pages. [June 6, 2009]
+* V1.35: Belarusian translation. [May 4, 2009]
+* V1.34: Turkish translation. [May 4, 2009]
+* V1.33: Added some HTML comments in the page with version number and ad block sequence number for easy trouble shooting. [May 1, 2009]
+* V1.32: More fixes to finally make the admin page totally "Valid XHTML 1.0 Transitional." Also releasing translation in Bahasa Indonesia.  [April 17, 2009]
+* V1.31: Minor bug fix (related to the number of ad blocks on a page). [April 13, 2009]
+* V1.30: Major overhaul of the interface. New clean look with javascript tooltips hiding details. New options to clean up the database entries and uninstall the plugin. [April 12, 2009]
+* V1.21: An option to show ads only in blog posts (and not on pages). [April 9, 2009]
+* V1.20: Internationalization, riding on the translations of Easy AdSenser. [April 4, 2009]
+* V1.11: Bug fix: the plugin wasn't gracefully handling posts with no custom fields. [March 10. 2009]
+* V1.10: Adding more control over displaying AdSense blocks in individual post. [March 8. 2009]
+* V1.01: A serious bug fix. [March 6, 2009]
+* V1.00: Initial release. [March 1, 2009]
